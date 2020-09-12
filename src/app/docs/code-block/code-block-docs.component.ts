d496397f522191b892fd95adbdb95a3629b0bc32
John Lyons <John.Lyons@blackbaud.me>
2020-09-11 23:09:02 -0400

    Set up JSDocs comments

---
+++ src/app/docs/code-block/code-block-docs.component.ts
@@ -0,0 +1,11 @@
+import {
+  ChangeDetectionStrategy,
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'app-code-block-docs',
+  templateUrl: './code-block-docs.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CodeBlockDocsComponent { }
