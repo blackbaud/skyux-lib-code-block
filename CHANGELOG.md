0b263816cb4a59289a765f57aa3d1bbc02349689
Steve Brush <steve.brush@blackbaud.com>
2022-06-13 16:08:45 -0400

    Add support for Angular 13

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 6.0.0-beta.0 (2022-06-13)
+
+- Added support for Angular 13. [#42](https://github.com/blackbaud/skyux-lib-clipboard/pull/42)
+
 # 4.0.4 (2021-12-17)
 
 - Fixed the code block component to handle dynamic updates to the `code` input property. [#55](https://github.com/blackbaud/skyux-lib-code-block/pull/55)
--- projects/code-block-showcase/karma.conf.js
+++ projects/code-block-showcase/karma.conf.js
@@ -8,7 +8,6 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
@@ -21,9 +20,6 @@ module.exports = function (config) {
       },
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
-    },
     coverageReporter: {
       dir: require('path').join(__dirname, '../../coverage/code-block-showcase'),
       subdir: '.',
@@ -32,7 +28,7 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
